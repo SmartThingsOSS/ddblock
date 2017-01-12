fb7c7ac4130fa7dabda7b65cfc06b526fce14911
Adam Mielke <adam.mielke@smartthings.com>
2017-01-12 16:14:41 -0600

    improvements

--- main.go
+++ main.go
@@ -6,45 +6,47 @@ import (
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 
-	"github.com/zencoder/ddbsync"
+	"github.com/SmartThingsOSS/ddbsync"
 )
 
 var (
 	table   = kingpin.Arg("table", "Name of DynamoDB table used for locking.").Required().String()
-	mutex   = kingpin.Arg("name", "Name of the lock to acquire.").Required().String()
-	ttl     = kingpin.Flag("ttl", "TTL to create lock with.").Default("10m").Duration()
+	mutex   = kingpin.Arg("name", "Name of the lock to acquire/unlock.").Required().String()
+	ttl     = kingpin.Flag("ttl", "TTL of the lock.").Default("10m").Duration()
 	timeout = kingpin.Flag("timeout", "Time out and fail if the lock isn't acquired within this duration.").Default("10m").Duration()
 	region  = kingpin.Flag("region", "AWS region in which to use DynamoDB").Default("us-east-1").String()
 	unlock  = kingpin.Flag("unlock", "Unlock the named mutex.").Short('u').Bool()
 )
 
 func main() {
-	kingpin.Version("0.0.1")
+	kingpin.Version("0.0.2")
 	kingpin.Parse()
 
 	m := ddbsync.NewMutex(
 		*mutex,
-		int64(*ttl),
+		int64(ttl.Seconds()),
 		ddbsync.NewDatabase(*table, *region, "", false),
 	)
 
+	var action func()
 	if *unlock {
-		m.Unlock()
-		return
+		action = m.Unlock
+	} else {
+		action = m.Lock
 	}
 
-	acquired := make(chan struct{})
+	done := make(chan struct{})
 	timer := time.After(*timeout)
 
 	go func() {
-		m.Lock()
-		acquired <- struct{}{}
+		action()
+		done <- struct{}{}
 	}()
 
 	select {
-	case <-acquired:
+	case <-done:
 		return
 	case <-timer:
-		log.Fatalf("failed to acquire lock within timeout window")
+		log.Fatalf("failed to complete lock action within timeout window")
 	}
 }
